b48c547e643d9437511aa6089abc24449490d1e1
Your Name <you@example.com>
2024-05-03 22:01:11 +0600

    bbackup

--- range-slider-with-indicator.js
+++ range-slider-with-indicator.js
@@ -3,13 +3,37 @@ window.addEventListener('load', () => {
     rangeSliders.forEach(initRangeSliderWithIndicator)
 }) 
 function initRangeSliderWithIndicator(rangeSliderWithIndicator) {
+    rangeSliderWithIndicator.innerHTML = `
+      <div class="indicator"></div>
+      <div class="slider">
+      <div class="runnable-track">
+       <div class="completed-track"></div> 
+        <div class="thumb"></div>
+        </div>
+      </div>
+    `
    const marksArray = JSON.parse(rangeSliderWithIndicator.getAttribute('data-indicator-marks'))
    setSldierControls(rangeSliderWithIndicator)
    setMarks(marksArray)
 }   
 
 function setSldierControls(rangeSliderWithIndicator) {
-  
+    let startX = 0
+ const thumbPointerMoveHandler = (e) => {
+    console.clear();
+    console.log(e.clientX - startX);
+     
+  }
+  const thumb = rangeSliderWithIndicator.querySelector('.slider > .runnable-track > .thumb')
+  thumb.addEventListener('pointerdown', (e) => {
+    startX = e.clientX
+  document.addEventListener('pointermove', thumbPointerMoveHandler)
+  })
+
+  document.addEventListener('pointerup', (e) => {
+   document.removeEventListener('pointermove', thumbPointerMoveHandler)
+ })
+ 
 }
 
 function setMarks(marksArray) {
