df504b252f2688756e8a4300edd60a37da528194
Your Name <you@example.com>
2024-05-04 10:34:32 +0600

    backup

--- range-slider-with-indicator.js
+++ range-slider-with-indicator.js
@@ -13,26 +13,51 @@ function initRangeSliderWithIndicator(rangeSliderWithIndicator) {
       </div>
     `
    const marksArray = JSON.parse(rangeSliderWithIndicator.getAttribute('data-indicator-marks'))
+   setHandlers(rangeSliderWithIndicator)
    setSldierControls(rangeSliderWithIndicator)
    setMarks(marksArray)
 }   
-
+function setHandlers(rangeSliderWithIndicator) {
+    const {oninput,onchange} = rangeSliderWithIndicator
+    if (oninput) {
+     rangeSliderWithIndicator.addEventListener('input', oninput)
+    }
+    if (onchange) {
+     rangeSliderWithIndicator.addEventListener('change', onchange)
+    }
+}
 function setSldierControls(rangeSliderWithIndicator) {
-    let startX = 0
+  const runnableTrack = rangeSliderWithIndicator.querySelector('.slider > .runnable-track')
+  const completedTrack = runnableTrack.querySelector('.completed-track')
+  const thumb = runnableTrack.querySelector('.thumb')
+  const runnableTrackWidth = runnableTrack.getBoundingClientRect().width
+  let startX = 0
+  let currentX = 0
  const thumbPointerMoveHandler = (e) => {
-    console.clear();
-    console.log(e.clientX - startX);
-     
-  }
-  const thumb = rangeSliderWithIndicator.querySelector('.slider > .runnable-track > .thumb')
-  thumb.addEventListener('pointerdown', (e) => {
-    startX = e.clientX
-  document.addEventListener('pointermove', thumbPointerMoveHandler)
-  })
+    const percent = ((e.clientX - startX) / runnableTrackWidth) * 100
+    const validPercent = Math.max(0, Math.min(percent, 100))
+    currentX = e.clientX
+    completedTrack.style.width = `${validPercent}%`
+    Object.getPrototypeOf(rangeSliderWithIndicator).value = validPercent
+    rangeSliderWithIndicator.dispatchEvent(new Event('input'))
+}
+ const thumbPointerUpHandler = (e) => {
+    console.log("pointer up");
+    rangeSliderWithIndicator.dispatchEvent(new Event('change'))
 
-  document.addEventListener('pointerup', (e) => {
-   document.removeEventListener('pointermove', thumbPointerMoveHandler)
+    document.removeEventListener('pointermove', thumbPointerMoveHandler)
+    document.removeEventListener('pointerup', thumbPointerUpHandler)
+ }
+
+  thumb.addEventListener('pointerdown', (e) => {
+    console.log(e);
+    const {width} = completedTrack.getBoundingClientRect()
+    startX =  e.clientX - width
+    document.addEventListener('pointermove', thumbPointerMoveHandler)
+    document.addEventListener('pointerup', thumbPointerUpHandler)
  })
+
+
  
 }
 
