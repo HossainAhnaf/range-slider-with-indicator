46768fb76dd7e810eb86171a90484e8e73cbaaed
HossainAhnaf <138277497+HossainAhnaf@users.noreply.github.com>
2024-05-08 21:20:13 +0600

    Update range-slider-with-indicator.js

--- range-slider-with-indicator.js
+++ range-slider-with-indicator.js
@@ -1,123 +1,141 @@
-window.addEventListener('load', () => {
-  const rangeSliders = document.querySelectorAll('div[role="range-slider-with-indicator"]')
-  rangeSliders.forEach(initRangeSliderWithIndicator)
-}) 
-function initRangeSliderWithIndicator(rangeSliderWithIndicator) {
-  const minValue = parseInt(rangeSliderWithIndicator.getAttribute('data-min')) || 0
-  const maxValue = parseInt(rangeSliderWithIndicator.getAttribute('data-max')) || 100
-  rangeSliderWithIndicator.innerHTML = `
-    <div class="indicator"></div>
-    <div class="slider">
-    <div class="runnable-track">
-     <div class="completed-track"></div> 
-      <div  class="thumb"></div>
+
+class RangeSliderWithIndicator {
+    constructor(element) {
+        this.element = element;
+        this.handlers = {
+            input: this.element.oninput,
+            change: this.element.onchange,
+        };
+        this.value = parseInt(this.element.getAttribute('data-value')) || 0;
+        this.minValue = parseInt(this.element.getAttribute('data-min')) || 0;
+        this.maxValue = parseInt(this.element.getAttribute('data-max')) || 100;
+        this.valueRange = this.maxValue - this.minValue;
+        this.initUI();
+        this.initHandlers();
+
+    }
+
+    initUI() {
+        this.element.innerHTML = `
+      <div class="indicator"></div>
+      <div class="slider">
+        <div class="runnable-track">
+          <div class="completed-track"></div>
+          <div class="thumb"></div>
+        </div>
       </div>
-    </div>
-  `
- setHandlers(rangeSliderWithIndicator)
- setSldierControls(rangeSliderWithIndicator,minValue,maxValue)
- setMarks(rangeSliderWithIndicator,minValue,maxValue)
-}   
-function setHandlers(rangeSliderWithIndicator) {
-  const {oninput,onchange} = rangeSliderWithIndicator
-  if (oninput) {
-   rangeSliderWithIndicator.addEventListener('r-s-w-i-input', oninput)
-  }
-  if (onchange) {
-   rangeSliderWithIndicator.addEventListener('r-s-w-i-change', onchange)
-  }
+    `;
+        this.runnableTrack = this.element.querySelector('.slider > .runnable-track');
+        this.runnableTrackRect = this.runnableTrack.getBoundingClientRect();
+        this.setSliderControls();
+        this.setMarks();
+        window.addEventListener("resize", () => {
+            this.runnableTrackRect = this.runnableTrack.getBoundingClientRect();
+            this.setSliderControls();
+            this.setMarks();
+        });
+    }
+
+    initHandlers() {
+        for (const key in this.handlers) {
+          const handler = this.handlers[key];
+          if (handler)
+          this.element.addEventListener(`r-s-w-i-${key}`, handler);
+        }
+    }
+    setSliderControls() {
+
+        const completedTrack = this.runnableTrack.querySelector('.completed-track');
+        const thumb = this.runnableTrack.querySelector('.thumb');
+
+        const thumbHalfWidth = thumb.getBoundingClientRect().width / 2;
+        const initialValueStr = this.element.getAttribute('data-value');
+        const initialValue = initialValueStr === null ? this.minValue : parseInt(initialValueStr);
+        let rangeCurrentValue = initialValue;
+        let valueDivision = this.runnableTrackRect.width / this.valueRange;
+        let startX = 0;
+        const initialCompletedWidth = valueDivision * (initialValue - this.minValue);
+        thumb.style.left = `${initialCompletedWidth - thumbHalfWidth}px`;
+        completedTrack.style.width = `${initialCompletedWidth}px`;
+
+        const dispatchInputHandler = () => {
+            this.element.value = rangeCurrentValue;
+            this.element.dispatchEvent(new Event('r-s-w-i-input'));
+        };
+
+        const thumbPointerMoveHandler = (e) => {
+            const movePx = e.clientX - startX;
+            const completedWidth = Math.max(0, Math.min(movePx, this.runnableTrackRect.width));
+            const newValue = Math.max(this.minValue, Math.min(this.maxValue, Math.round(this.minValue + (movePx / valueDivision))));
+            if (newValue !== rangeCurrentValue) {
+                rangeCurrentValue = newValue;
+                dispatchInputHandler();
+            }
+            thumb.style.left = `${completedWidth - thumbHalfWidth}px`;
+            completedTrack.style.width = `${completedWidth}px`;
+        };
+
+        const thumbPointerUpHandler = (e) => {
+            this.element.dispatchEvent(new Event('r-s-w-i-change'));
+            document.removeEventListener('pointermove', thumbPointerMoveHandler);
+            document.removeEventListener('pointerup', thumbPointerUpHandler);
+        };
+
+        thumb.addEventListener('pointerdown', (e) => {
+            const { width } = completedTrack.getBoundingClientRect();
+            startX = e.clientX - width;
+            document.addEventListener('pointermove', thumbPointerMoveHandler);
+            document.addEventListener('pointerup', thumbPointerUpHandler);
+        });
+
+        this.runnableTrack.addEventListener('click', (e) => {
+            const px = e.clientX - this.runnableTrackRect.left;
+            
+        });
+
+        thumb.ondragstart = (e) => e.preventDefault();
+    }
+
+    setMarks() {
+
+        const indicator = this.element.querySelector('.indicator');
+        indicator.innerHTML = '';
+        const rangeSliderWithIndicatorWidth = this.element.getBoundingClientRect().width;
+        const totalMarks = Math.round(rangeSliderWithIndicatorWidth / 50);
+        const marksTextArray = this.getMarksText(this.minValue, this.valueRange, totalMarks);
+        const distance = rangeSliderWithIndicatorWidth / totalMarks;
+
+        for (let i = 0; i < marksTextArray.length; i++) {
+            const marksText = document.createElement('div');
+            const marksStop = document.createElement('div');
+            marksText.classList.add('marks-text');
+            marksText.innerText = marksTextArray[i];
+            marksStop.classList.add('marks-stop');
+            indicator.append(marksText, marksStop);
+            const leftPx = (marksTextArray[i] - this.minValue) * (this.runnableTrackRect.width / this.valueRange);
+            const { width } = marksText.getBoundingClientRect();
+            marksText.style.left = `${leftPx - width / 2}px`;
+            marksStop.style.left = `${leftPx}px`;
+        }
+    }
+
+    getMarksText(minValue, valueRange, totalMarks) {
+        const marksTextArray = [minValue];
+        const increace = valueRange / totalMarks;
+        let multiply = 1;
+        for (let i = 1; i <= totalMarks; i++, multiply++) {
+            const marksText = minValue + increace * multiply;
+            marksTextArray.push(Math.round(marksText));
+        }
+        return marksTextArray;
+    }
 }
-function setSldierControls(rangeSliderWithIndicator,minValue,maxValue) {
-const runnableTrack = rangeSliderWithIndicator.querySelector('.slider > .runnable-track')
-const completedTrack = runnableTrack.querySelector('.completed-track')
-const thumb = runnableTrack.querySelector('.thumb')
-let runnableTrackWidth = runnableTrack.getBoundingClientRect().width
-const initialValueStr = rangeSliderWithIndicator.getAttribute('data-value') 
-const initialValue = initialValueStr === null ? minValue :  parseInt(initialValueStr)
-
-let rangeCurrentValue = initialValue
-const valueRange = maxValue - minValue
-let division = runnableTrackWidth / valueRange
-let multiply = initialValue - minValue
-let pxToNext = division * multiply
-let pxToPrev = pxToNext - division
-let startX = 0
-completedTrack.style.width = `${division * multiply}px`
-
-window.addEventListener("resize",()=>{
-   runnableTrackWidth = runnableTrack.getBoundingClientRect().width
-    division = runnableTrackWidth / valueRange
-    pxToNext = division * multiply
-    pxToPrev = pxToNext - division
-   completedTrack.style.width = `${division * multiply}px`
-
-})
-
-const dispatchInputHandler = ()=>{
-  Object.getPrototypeOf(rangeSliderWithIndicator).value = rangeCurrentValue
-  rangeSliderWithIndicator.dispatchEvent(new Event('r-s-w-i-input'))
-}
-const canMoveNext = (movePx)=>{
- return  (rangeCurrentValue < maxValue && movePx >= pxToNext)
-}
-const canMovePrev = (movePx)=> {
- return  (rangeCurrentValue > minValue && movePx <= pxToPrev)
-}
-const thumbPointerMoveHandler = (e) => {
-  const movePx = (e.clientX - startX)
-  const _canMoveNext = canMoveNext(movePx)
-  const _canMovePrev = canMovePrev(movePx)
- while (canMoveNext(movePx) || canMovePrev(movePx)) {
-  if (_canMoveNext){
-  completedTrack.style.width = `${pxToNext}px`
-  pxToNext = division * ++multiply
-  pxToPrev =  pxToNext - division
-  rangeCurrentValue++
-  dispatchInputHandler()
- }else if (_canMovePrev) {
-  completedTrack.style.width = `${pxToPrev}px`
-  pxToNext = division * --multiply
-  pxToPrev =  pxToNext - division
-  rangeCurrentValue--
-  dispatchInputHandler()
- }
- }
-
-
-}
-const thumbPointerUpHandler = (e) => {
-  rangeSliderWithIndicator.dispatchEvent(new Event('r-s-w-i-change'))
-
-  document.removeEventListener('pointermove', thumbPointerMoveHandler)
-  document.removeEventListener('pointerup', thumbPointerUpHandler)
-}
-thumb.addEventListener('pointerdown', (e) => {
-  console.log(e);
-  const {width} = completedTrack.getBoundingClientRect()
-  startX =  e.clientX - width
-  document.addEventListener('pointermove', thumbPointerMoveHandler)
-  document.addEventListener('pointerup', thumbPointerUpHandler)
-})
-thumb.ondragstart = (e) => e.preventDefault()
 
 
 
-}
-
-function setMarks(rangeSliderWithIndicator,minValue,maxValue) { 
-
-//    console.log(marksArray);
-
-//    const indicator = document.querySelector('.indicator')
-//    indicator.innerHTML = marksArray.map(i => `<div class="mark">${i}</div>`).join('')
- const rangeSliderWithIndicatorWidth =rangeSliderWithIndicator.getBoundingClientRect().width
- const totalMarks = Math.round(rangeSliderWithIndicatorWidth / 50)
- const marksArray = []
-
- for (let i = 0; i < totalMarks; i++) {
-   console.log((90 / totalMarks) + (marksArray[i - 1] || 0))
-  marksArray.push((90 / totalMarks) + (marksArray[i - 1] || 0) )
- }
-
- console.log(marksArray,totalMarks) 
-}
+window.addEventListener('load', () => {
+    const rangeSliders = document.querySelectorAll(
+        'div[role="range-slider-with-indicator"]'
+    );
+    rangeSliders.forEach(elm => new RangeSliderWithIndicator(elm));
+});
